2cf4c2098f2996a2730c54f860485ac3670fb632
George So <George So@10.6.1.114>
2023-03-30 14:23:55 -0400

    March 30 2023

--- Unit_Converter/src/application/Main.java
+++ Unit_Converter/src/application/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Unit converter");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
